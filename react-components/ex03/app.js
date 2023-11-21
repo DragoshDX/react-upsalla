7d26598e2e1a7fe6394cf2f41d9d642f0a4aeb57
Dragos <dragos.i@adoreme.com>
2023-11-21 20:29:05 +0200

    2030

--- react-components/ex03/app.js
+++ react-components/ex03/app.js
@@ -22,4 +22,28 @@ const person = {
   },
 };
 
+console.warn(`
+  Folosind Object.values(), afiseaza o
+  lista inversata cu numele complet inversat al prietenilor.
+`);
+Object.values(person.friends)
+  .reverse()
+  .forEach(({ surname, name }) => {
+    console.log(`${surname} ${name}`);
+  });
 
+console.warn(`Afiseaza propozitia: “Prietenii mei sunt Larry,
+  Steven si Carol.” folosind Object.values()
+`);
+console.log(
+  Object.values(person.friends).reduce((sentence, friend, index, friends) => {
+    const { name } = friend;
+    const length = friends.length;
+    const punctuation =
+      length - 1 === index ? '.' : length - 2 === index ? ' si ' : ', ';
+
+    sentence += `${name}${punctuation}`;
+
+    return sentence;
+  }, 'Prietenii mei sunt '),
+);
