608d0cca62be25157371ebf9cb24c8080f114692
Dragos <dragos.i@adoreme.com>
2023-11-23 21:23:15 +0200

    2123

---
+++ app-contact-manager/modules/searchForm.js
@@ -0,0 +1,12 @@
+const searchForm = document.querySelector('.search-form');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const queryInput = form.q;
+  const searchValue = queryInput.value;
+
+  queryInput.value = '';
+});
+
+export default searchForm;
